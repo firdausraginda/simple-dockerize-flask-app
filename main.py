9ae2df5f737b57e2ccccd3c75e3cac916d4277b3
firdausraginda <ragindafirdaus01@gmail.com>
2024-04-03 18:02:29 +0700

    update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_geek():
     env_var = os.getenv("HELLO_NAME", "world")
-    return {"message": f"hola hola adios {env_var}!"}
+    return {"message": f"hai {env_var}!"}
 
 
 if __name__ == "__main__":
